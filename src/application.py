b732f96417514ad815cf745ab747cd4db5da3766
sauce21 <sho11decade@gmail.com>
2024-06-27 23:50:58 +0900

    chore: Update file upload functionality and add image validation

--- src/application.py
+++ src/application.py
@@ -1,8 +1,23 @@
-from bottle import route, run, static_file
- 
-@route('/file/<filename:path>')
-def static(filename):
-    return static_file(filename, root="D:\projects\spot-the-difference-generate\static\\")
- 
+from bottle import route, run, template, request, static_file, url, get, post, response, error, abort, redirect, os
+static_path = 'D:\projects\spot-the-difference-generate\static\\'
+@route('/machigai/<filename>')
+def home(filename):
+    return static_file(filename, root=static_path)
+
+@route('/process', method='POST')
+def process():
+    data = request.files.getall('up')
+    for upload in data:
+        name, ext = os.path.splitext(upload.filename)
+        if ext not in ('.png', '.jpg', '.jpeg'):
+            return 'File extension not allowed.'
+
+    for upload in data:
+        print(upload.filename)
+        save_path = upload.filename
+        upload.save(save_path)
+    return 'OK'
+
+
 if __name__ == "__main__":
     run(host='localhost', port=80, debug=True, reloader=True)
